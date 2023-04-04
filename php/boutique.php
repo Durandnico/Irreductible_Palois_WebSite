a7a7ed709f98a31839b741a4607a45616def448b
Durandnico <nicod4956@gmail.com>
2023-04-04 18:51:18 +0200

    full php line, no html

---
+++ php/boutique.php
@@ -0,0 +1,107 @@
+<?php
+
+    session_start();
+    $cat = $_GET['cat'];
+
+    if( !isset($_SESSION['shop_data'][$cat]) ) 
+        header('Location: ../index.php?error=1');
+
+    $data = $_SESSION['shop_data'][$cat];
+?>
+
+
+<!-- Path: phtml part -->
+<html>
+    <head>
+        <title>Les irréductibles Palois</title>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="../css/general.css">
+        <link rel="stylesheet" href="../css/shop.css">
+    </head>
+    <div class="header_filter">
+        <?php
+        require '../inc/header.html';
+        ?>
+    </div>
+
+    <body>
+
+    <div class="Zoomed-product" hidden onblur="close_zoomed_product(this)">
+            <div class="product-content">
+                <div id="img-background" class="product-content-image"></div>
+                <div class="product-content-info">
+                    <div id="intels" class="text">
+                        <h1>Casque polonais (légérement abimé)</h1>
+                        <h3>Utile pour initiation de famille!</h3>
+                        <!--<h4>conseil d'utilisation: préparer 5 shooters sur une table, munissez-vous d'objets insolites (pied de table, panneau de circulation, gant de boxe) et surtout n'oubliez pas d'immortaliser le moment</h4> -->
+                        <h4>More description to add</h4>
+                        <h2>69€</h2>
+                    </div>
+
+                    <div class="quantite">
+                        <fieldset class="quantity">
+                            <legend>Quantité</legend>
+                            <input id="qte" type="number" class="number" value=1 />
+                            <div class="btnplusmoins"> 
+                                <button class="plus" onclick="plusmoins(+1)"><div class="my_filter">+</div></button>
+                                <button class="minus" onclick="plusmoins(-1)"><div class="my_filter">-</div></button>
+                            </div>
+                        </fieldset>
+                        <span> quantité max : <span id="q_max"></span></span>
+                    </div>
+                    <button><div class="my_filter"><p>Acheter</p></div></button>
+                </div>    
+            </div>
+            <button id="quit" onclick="close_zoomed_product()">X</button>
+        </div>
+
+        <div class="big_filter">
+
+            <div class="main-presentation">
+                
+                <div class="banner_intel">
+                    <h1><?php echo $_SESSION['shop_data']['Header'][$cat]['titre'] ?></h1>
+                    <p><?php echo $_SESSION['shop_data']['Header'][$cat]['ss-titre'] ?></p>
+                    <label style="position: absolute; top:15; right:15;" class="switch">
+                        <input onclick="switch_quantity('<?php echo $_SESSION['shop_data']['header'][$cat]['quantity'] ?>');" type="checkbox" />
+                        <span></span>
+                    </label>
+                    <span style="position: absolute; bottom: 5; right: 12;">Quantité</span>
+                </div>
+                    
+                <div class="vitrine">
+                    <table>
+                        <tbody>
+                            <?php
+                                $i = 0;
+                                foreach ($data as $key => $value) {
+                                    if($i % 4 == 0) echo '<tr>';
+                                    echo '<td>';
+                                    echo '<div class="product">';
+                                    echo '<div class="product-image">';
+                                    echo '<img src="'.$value['img'].'" alt="'.$value['alt'].'" />';
+                                    echo '</div>';
+                                    echo '<div class="content">';
+                                    echo '<h4>'.$value['name'].'</h4>';
+                                    echo '<h5>'.$value['price'].'€</h5>';
+                                    echo '<h3>'.$value['description'].'</h3>';
+                                    echo '<span class="'.$_SESSION['shop_data']['Header'][$cat]['quantity'].'">'.$value['quantity'].'</span>';
+                                    echo '<button onclick="zoom_product(this)"><div class="my_filter"><p>Acheter</p></div></button>';
+                                    echo '</div>';
+                                    echo '</div>';
+                                    echo '</td>';
+                                    if($i % 4 == 0) echo '</tr>';
+                                    $i++;
+                                }
+                            ?>
+                        </tbody>
+                    </table>     
+                </div>
+            </div>
+        </div>
+        
+<?php
+    require '../inc/footer.html';
+?>
+    </body>
+</html>
