5f767c2658ff441a25be88f1c60f599720f7050d
durandnico <durandnico@cy-tech.fr>
2023-04-08 17:13:46 +0200

    Ajout dernière data + correction de bug

--- php/boutique.php
+++ php/boutique.php
@@ -75,17 +75,19 @@
                         <tbody>
                             <?php
                                 $i=0;
+                                $id = "";
+                                if ($cat == "alcool") $id = 'id="contain" ';
                                 foreach ($data as $key => $value) {
                                     if($i % 4 == 0) {echo '<tr>';}
                                     echo '<td>';
                                     echo '  <div class="product">';
                                     echo '      <div class="product-img">';
-                                    echo '          <img src="'.$value['image'].'" alt="'.$value['alt'].'" />';
+                                    echo '          <img src="'.$value['image'].'" ' . $id . ' alt="'.$value['alt'].'" />';
                                     echo '      </div>';
                                     echo '      <div class="content">';
                                     echo '          <h4>'.$value['name'].'</h4>';
-                                    echo '          <h5>'.$value['price'].'€</h5>';
-                                    echo '          <h3>'.$value['short-description'].'</h3>';
+                                    echo '          <h5>'.$value['short-description'].'€</h5>';
+                                    echo '          <h3>'.$value['price'].'</h3>';
                                     echo '          <span class="'.$_SESSION['shop_data']['Header'][$cat]['quantity'].'" style="visibility:hidden;">Quantité max : '.$value['quantity'].'</span>';
                                     echo '          <button onclick="zoom_product(this)"><div class="my_filter"><p>Ajouter au panier</p></div></button>';
                                     echo '      </div>';
