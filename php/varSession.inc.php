5ef4ff514378b5ee6aebda44a58f090076eed09e
durandnico <durandnico@cy-tech.fr>
2023-04-12 18:05:22 +0200

    refactor folder

--- php/varSession.inc.php
+++ php/varSession.inc.php
@@ -1,11 +1,16 @@
 <?php
     session_start();
 
-    /* initialisation de la variable de session from json file */
+    /* initializing shop data from json file */
     if (!isset($_SESSION['shop_data'])) {
-        $json = file_get_contents('data/json/shop.json');
+        $json = file_get_contents('data/shop.json');
         $data = json_decode($json, true);
         $_SESSION['shop_data'] = $data;
+        
     }
      
+    /* initializing other session var  */
+    if (!isset($_SESSION['user_data']))
+        $_SESSION['user_data'] = array();
+
 ?>
